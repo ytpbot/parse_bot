2477b30b8df6c77e3b4d565297d6d23b036f14bc
vasilev180470 <valerick180470@gmail.com>
2023-09-02 22:13:51 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,2 @@
+TOKEN = '5966027378:AAHZPBBq8Paig13Z6yAVWXkVl50JDcA-cGs'
+URL_APP = 'https://pythonytbot-vasilev180470.amvera.io'
---
+++ ytpBot.py
@@ -0,0 +1,81 @@
+from youtube_search import YoutubeSearch
+import config
+
+
+from aiogram import Bot, types, utils
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from aiogram.types import InputTextMessageContent, InlineQueryResultArticle, input_message_content
+import hashlib
+import os
+
+
+def searcher(text):
+    res = YoutubeSearch(text, max_results=10).to_dict()
+    return res
+
+
+bot = Bot(token=config.TOKEN)
+dp = Dispatcher(bot)
+
+async def on_startup(dp):
+    await bot.set_webhook(config.URL_APP)
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
+
+@dp.message_handler()
+async def start(message: types.Message):
+    await message.answer('Это инлайн бот!')
+
+@dp.inline_handler()
+async def inline_handler(query : types.InlineQuery):
+    text = query.query or 'echo'
+    links = searcher(text)
+
+    articles = [types.InlineQueryResultArticle(
+        id = hashlib.md5(f'{link["id"]}'.encode()).hexdigest(),
+        title = f'{link["title"]}',
+        url = f'https://www.youtube.com/watch?v={link["id"]}',
+        thumb_url = f'{link["thumbnails"][0]}',
+        input_message_content=types.InputTextMessageContent(
+                            message_text=f'https://www.youtube.com/watch?v={link["id"]}')
+    ) for link in links]
+
+    await query.answer(articles, cache_time=60, is_personal=True)
+
+
+executor.start_webhook(
+    dispatcher = dp,
+    webhook_path='',
+    on_startup=on_startup,
+    on_shutdown=on_shutdown,
+    skip_updates=True,
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 5000)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
